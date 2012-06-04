98905d69bfe07fbcfa8bc1c0693aa4a7dc019163
Erik Underbjerg <erik@underbjerg.com>
2012-06-04 22:38:21 +0200

    Generated rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KayakClub::Application.config.secret_token = 'ba04e8ae9925f61c840eb53e74308a2e424aa1c55968bfc46b1fde0880561e099d4a3435e621403a8e8b9dedf253b5e669afa1e0748d963cc3d215fd49f682f8'
