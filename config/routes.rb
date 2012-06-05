94990e75e4122b0ed5178ac90900dec26f447508
Erik Underbjerg <erik@underbjerg.com>
2012-06-05 13:58:24 +0200

    Better localization

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ KayakClub::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'event_occurrences#index'
 
   # See how all your routes lay out with "rake routes"
 
