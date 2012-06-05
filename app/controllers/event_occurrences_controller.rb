023304709f1772f93073cbc3e1b060ccc310d554
Erik Underbjerg <erik@underbjerg.com>
2012-06-05 15:04:47 +0200

    Sorting of events, and phone links

--- app/controllers/event_occurrences_controller.rb
+++ app/controllers/event_occurrences_controller.rb
@@ -3,7 +3,7 @@ class EventOccurrencesController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @event_occurrences = EventOccurrence.all
+    @event_occurrences = EventOccurrence.order("start_at")
   end
 
   def show
