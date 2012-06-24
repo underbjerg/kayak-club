7b2afb482033e0d17d916c1a83214bb3861cff82
Erik Underbjerg <erik@underbjerg.com>
2012-06-24 13:38:34 +0200

    Configured email settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 KayakClub::Application.initialize!
+
+# Configure mail settings
+ActionMailer::Base.smtp_settings = {
+    :address                => "smtp.gmail.com",
+    :port                   => 587,
+    :domain                 => "underbjerg.com",
+    :user_name              => "kajakpiloterne@underbjerg.com",
+    :password               => "havkajak",
+    :authentication         => "plain",
+    :enable_starttls_auto   => true
+}
